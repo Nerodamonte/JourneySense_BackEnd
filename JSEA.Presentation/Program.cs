81b704f8c0d4f44ddf744b0c876a112c46e8237d
Boisopsop <nhathoang1645@gmail.com>
2026-03-17 17:39:36 +0700

    Add Get User Package

--- JSEA.Presentation/Program.cs
+++ JSEA.Presentation/Program.cs
@@ -20,6 +20,7 @@ using JSEA_Application.Services.Journey;
 using JSEA_Application.Services.Profile;
 using JSEA_Application.Services.Category;
 using JSEA_Application.Services.Package;
+using JSEA_Application.Services.UserPackage;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -71,6 +72,8 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IPackageRepository, PackageRepository>();
 builder.Services.AddScoped<IPackageService, PackageService>();
+builder.Services.AddScoped<IUserPackageRepository, UserPackageRepository>();
+builder.Services.AddScoped<IUserPackageService, UserPackageService>();
 
 // Journey Setup (Goong Maps)
 builder.Services.AddHttpClient();
---
+++ JSEA_Application/Interfaces/IUserPackageService.cs
@@ -0,0 +1,9 @@
+using JSEA_Application.DTOs.Respone.UserPackage;
+
+namespace JSEA_Application.Interfaces;
+
+public interface IUserPackageService
+{
+    Task<UserCurrentPackageDto?> GetCurrentPackageByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+}
+
